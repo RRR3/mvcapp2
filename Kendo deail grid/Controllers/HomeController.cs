482108fcccf4402b450951175693a895b4473d9b
rrr3 <ravi.rr@sonata-software.com>
2013-11-22 09:16:37 +0530

    Some changes

---
+++ Kendo deail grid/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Kendo_deail_grid.Controllers
+{
+    public class HomeController
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
