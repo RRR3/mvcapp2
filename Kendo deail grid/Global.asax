beb7521004352a675ede9e7a1ab2ec611b5edca8
rrr3 <ravi.rr@sonata-software.com>
2013-11-22 17:37:47 +0530

    F*** O**

--- Kendo deail grid/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kendo_deail_grid.MvcApplication" Language="C#" %>
--- Kendo deail grid/Scripts/DatePickerHookup.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="jquery-1.9.2.js" />
-/// <reference path="jquery-ui-1.10.2.js" />
-$(function () {$(":input[data-datapicker=true]").datepicker();
-});
